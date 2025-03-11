6ed64728644a6e67aac30ee07f621a98b36fc1aa
ferhanabaci <ferhanabacii@gmail.com>
2025-03-12 00:52:19 +0300

    Rapid API test

---
+++ NetCoreAI.Project3_RapidAPI/ViewModel/ApiSeriesViewModel.cs
@@ -0,0 +1,14 @@
+﻿
+namespace NetCoreAI.Project3_RapidAPI.ViewModel
+{
+    class ApiSeriesViewModel
+    {
+        
+            public int rank { get; set; }
+            public string title { get; set; }
+            public float rating { get; set; }
+            public string year { get; set; }
+        
+
+    }
+}
