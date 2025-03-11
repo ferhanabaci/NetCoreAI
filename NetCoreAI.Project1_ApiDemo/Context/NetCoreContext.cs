1301ea5f0e419764c03621025b1e3902285bfd06
ferhanabaci <ferhan.abaci@santsg.com>
2025-03-11 23:49:18 +0300

    Add project files.

---
+++ NetCoreAI.Project1_ApiDemo/Context/NetCoreContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NetCoreAI.Project1_ApiDemo.Entities;
+
+namespace NetCoreAI.Project1_ApiDemo.Context
+{
+    public class NetCoreContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost;Database=NetCoreAI;User Id=SA;Password=S1611n2301T;TrustServerCertificate=True");
+        }
+
+        public DbSet<Customer> Customers { get; set; }     
+    }
+}
---
+++ NetCoreAI.Project1_ApiDemo/Controllers/CustomerController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NetCoreAI.Project1_ApiDemo.Context;
+using NetCoreAI.Project1_ApiDemo.Entities;
+
+namespace NetCoreAI.Project1_ApiDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private NetCoreContext _context;
+
+        public CustomerController(NetCoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult CustomerList()
+        {
+            var values = _context.Customers.ToList();
+            return Ok(values);
+
+        }
+        [HttpPost]
+        public IActionResult AddCustomer(Customer customer)
+        {
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+            return Ok("Müşteri Eklendi ");
+                
+        }
+        [HttpDelete]
+        public IActionResult DeleteCustomer(int id)
+        {
+            var values =_context.Customers.Find(id);
+            _context.Customers.Remove(values);
+            _context.SaveChanges();
+            return Ok();
+
+        }
+        [HttpPut]
+        public IActionResult UpdateCustomer(Customer customer)
+        {
+            _context.Customers.Update(customer);
+            _context.SaveChanges();
+            return Ok("müsteri eklendi");
+            
+        }
+    }
+}
+    
